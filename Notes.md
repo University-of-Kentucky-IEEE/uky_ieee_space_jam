82b95a2552795150ebd139c64baf7549bbd2477b
Ben Carey <benjamin.james.carey3@gmail.com>
2026-03-12 18:37:27 -0400

    first animation

---
+++ Notes.md
@@ -0,0 +1,10 @@
+# Ideas to add:
+
+We're thinking of being on a spaceship that has many rooms that need to be cleared. They each have a minigame
+associated. Some ideas for these rooms are:
+- 2D platformer room
+- 
+- Rap battle dance off
+
+Some general themes:
+- Jam controlled by AI that needs to be destroyed.
--- Player.cs
+++ Player.cs
@@ -1,6 +1,47 @@
 using Godot;
-using System;
+
+namespace UKYIEEESpaceJam;
 
 public partial class Player : CharacterBody2D
 {
+	private const int MaxSpeed = 100;
+
+	private AnimatedSprite2D Animation = null;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		
+		Animation = (AnimatedSprite2D)FindChild("AnimatedSprite2D");
+		
+		GD.Print("hello!");
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+
+		SetVelocity(Vector2.Zero);
+		
+		// Make a 2D RPG-style mover
+		if (Input.IsActionPressed("MoveLeft"))
+			SetVelocity(GetVelocity() - new Vector2(MaxSpeed, 0));
+		if (Input.IsActionPressed("MoveRight"))
+			SetVelocity(GetVelocity() + new Vector2(MaxSpeed, 0));
+		if (Input.IsActionPressed("MoveUp"))
+			SetVelocity(GetVelocity() - new Vector2(0, MaxSpeed));
+		if (Input.IsActionPressed("MoveDown"))
+			SetVelocity(GetVelocity() + new Vector2(0, MaxSpeed));
+
+		if (GetVelocity() != Vector2.Zero)
+		{
+			Animation.Play();
+		}
+		else
+		{
+			Animation.Stop();
+		}
+		
+		MoveAndSlide();
+	}
 }
