4c58c4a6e13b4855abf70c80dc8c765aaa8244e4
BeanFeed <two.austin.two@gmail.com>
2026-03-13 20:17:32 -0500

    fix tings

--- assets/scripts/Hand.cs
+++ assets/scripts/Hand.cs
@@ -6,7 +6,7 @@ public partial class Hand : Node2D
 {
 	
 	private Node2D _mountPoint;
-	private Node2D _heldItem;
+	public Item? HeldItem { get; private set; }
 
 	public override void _Ready()
 	{
@@ -14,19 +14,22 @@ public partial class Hand : Node2D
 	}
 	public override void _Process(double delta)
 	{
-		Vector2 localMousePosition = GetLocalMousePosition();
-		float rads = MathF.Atan2(-localMousePosition.Y, localMousePosition.X);
+		Vector2 mousePosition = GetGlobalMousePosition();
+		float rads = MathF.Atan2(-(mousePosition.Y - GetGlobalPosition().Y), mousePosition.X - GetGlobalPosition().X);
 
 		Rotation = rads;
 	}
 
 	public void PickupItem(Item item)
 	{
+		if (HeldItem != null) return;
 		item.Reparent(_mountPoint);
+		HeldItem = item;
 	}
 
 	public void DropItem(Item item)
 	{
+		if(item == null) return;
 		item.Reparent(GetTree().GetCurrentScene(), true);
 		item.Rotation = 0;
 	}
