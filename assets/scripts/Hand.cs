b84eadc2efca5e9d2410bdb0164a32026cd5bc99
BeanFeed <two.austin.two@gmail.com>
2026-03-13 20:03:15 -0500

    balls x2

---
+++ assets/scripts/Hand.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+using UKYIEEESpaceJam;
+
+public partial class Hand : Node2D
+{
+	
+	private Node2D _mountPoint;
+	private Node2D _heldItem;
+
+	public override void _Ready()
+	{
+		_mountPoint = GetNode<Node2D>("Mount");
+	}
+	public override void _Process(double delta)
+	{
+		Vector2 localMousePosition = GetLocalMousePosition();
+		float rads = MathF.Atan2(-localMousePosition.Y, localMousePosition.X);
+
+		Rotation = rads;
+	}
+
+	public void PickupItem(Item item)
+	{
+		item.Reparent(_mountPoint);
+	}
+}
